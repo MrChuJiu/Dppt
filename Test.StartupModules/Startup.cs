ea3e848ab2444773ed8276dcf11c704c089907c1
韩冬 <377749229@qq.com>
2020-09-03 23:09:09 +0800

    添加熊猫的模块化测试

--- Test.StartupModules/Startup.cs
+++ Test.StartupModules/Startup.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Easy.Core.Flow.RivenModular;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -25,6 +26,7 @@ namespace Test.StartupModules
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddRivenModule<MyAppStartupModule>(Configuration);
             services.AddControllers();
             Console.WriteLine("Startup----ConfigureServices");
         }
