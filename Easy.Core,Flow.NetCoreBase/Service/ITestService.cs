bec1087eb7e216ef09ddb48e2101ba810c39b030
HDONG <377749229@qq.com>
2020-11-20 21:50:27 +0800

    晚上ML的小Demo

---
+++ Easy.Core,Flow.NetCoreBase/Service/ITestService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Easy.Core_Flow.NetCoreBase.Service
+{
+    public interface ITestService
+    {
+        string AddProduct();
+    }
+}
---
+++ Easy.Core,Flow.NetCoreBase/Service/ServiceBase.cs
@@ -0,0 +1,25 @@
+﻿using Easy.Core_Flow.NetCoreBase.BaaeContextAccessor;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Easy.Core_Flow.NetCoreBase.Service
+{
+    public class ServiceBase
+    {
+        /// <summary>
+        /// 身份信息
+        /// </summary>
+        protected IClaimsAccessor Claims { get; set; }
+
+        /// <summary>
+        /// cotr
+        /// </summary>
+        protected ServiceBase()
+        {
+            Claims = ServiceProviderInstance.Instance.GetRequiredService<IClaimsAccessor>();
+        }
+    }
+}
