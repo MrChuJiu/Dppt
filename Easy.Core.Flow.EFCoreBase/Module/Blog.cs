1ab5e31c393a2119777f3dbdf4518afb9bc04bb4
HDONG <377749229@qq.com>
2021-01-26 21:24:43 +0800

    EF基础部分

---
+++ Easy.Core.Flow.EFCoreBase/Module/Blog.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Easy.Core.Flow.EFCoreBase.Module
+{
+    public class Blog
+    {
+        public int BlogId { get; set; }
+        public string Url { get; set; }
+    }
+}
