ad2f654fca1fe0c6655df55a9211668c64e78fce
MrChuJiu <377749229@qq.com>
2022-03-24 17:24:34 +0800

    修改博客园修改自己文章字符

---
+++ old/Easy.Core.Flow.cnblogsUpdate/Program.cs
@@ -0,0 +1,337 @@
+﻿using HtmlAgilityPack;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Net.Http;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace Easy.Core.Flow.cnblogsUpdate
+{
+    internal class Program
+    {
+        static string Cookie = "";
+        static string XsrfCookie = "";
+
+        static string SoureStr = "https://git.imweb.io/hdong/ImageBed/raw/master/";
+        static string TargetStr = "https://gitee.com/github-HD/image-bed/raw/master/";
+
+        static int Page = 7;
+        static List<string> UrlList = new List<string>();
+
+        static async Task Main(string[] args)
+        {
+
+            var handler = new HttpClientHandler() { UseCookies = false };
+            var client = new HttpClient(handler);
+
+            for (int i = 1; i <= Page; i++)
+            {
+                var message = new HttpRequestMessage(HttpMethod.Get, $"https://i.cnblogs.com/api/posts/list?p={i}&cid=&tid=&t=1&cfg=0&search=&orderBy=&scid=");
+                message.Headers.Add("Cookie", Cookie);
+                var result = await client.SendAsync(message);
+                result.EnsureSuccessStatusCode();
+                var value = await result.Content.ReadAsStringAsync();
+                var rootData = JsonSerializer.Deserialize<Root>(value);
+
+                foreach (var item in rootData.postList)
+                {
+                    UrlList.Add("https://i.cnblogs.com/api/posts/" + item.id);
+                }
+            }
+
+            foreach (var item in UrlList) {
+
+                var message = new HttpRequestMessage(HttpMethod.Get, item);
+                message.Headers.Add("Cookie", Cookie);
+                var result = await client.SendAsync(message);
+                result.EnsureSuccessStatusCode();
+                var value = await result.Content.ReadAsStringAsync();
+                var rootData = JsonSerializer.Deserialize<RootPost>(value);
+                rootData.blogPost.postBody = rootData.blogPost.postBody.Replace(SoureStr, TargetStr);
+
+                var str = JsonSerializer.Serialize(rootData.blogPost);
+                HttpContent content = new StringContent(str);
+                content.Headers.Add("cookie", Cookie);
+                content.Headers.Add("x-xsrf-token", XsrfCookie);
+                content.Headers.Add("origin", "https://i.cnblogs.com");
+                content.Headers.ContentType = new System.Net.Http.Headers.MediaTypeHeaderValue("application/json");
+                HttpResponseMessage response = await client.PostAsync("https://i.cnblogs.com/api/posts", content);//改成自己的
+                response.EnsureSuccessStatusCode();//用来抛异常的
+
+            }
+
+
+            Console.Read();
+        }
+    }
+
+
+
+    //如果好用，请收藏地址，帮忙分享。
+    public class PostListItem
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        public int id { get; set; }
+        /// <summary>
+        /// 浅讲.Net 6 之 WebApplicationBuilder
+        /// </summary>
+        public string title { get; set; }
+        /// <summary>
+        /// 
+        /// </summary>
+        public string url { get; set; }
+        /// <summary>
+        /// 
+        /// </summary>
+        public bool isPublished { get; set; }
+        /// <summary>
+        /// 
+        /// </summary>
+        public bool isDraft { get; set; }
+        /// <summary>
+        /// 
+        /// </summary>
+        public int feedBackCount { get; set; }
+        /// <summary>
+        /// 
+        /// </summary>
+        public int webCount { get; set; }
+        /// <summary>
+        /// 
+        /// </summary>
+        public int aggCount { get; set; }
+        /// <summary>
+        /// 
+        /// </summary>
+        public int viewCount { get; set; }
+        /// <summary>
+        /// 
+        /// </summary>
+        public string datePublished { get; set; }
+        /// <summary>
+        /// 
+        /// </summary>
+        public string entryName { get; set; }
+        /// <summary>
+        /// 
+        /// </summary>
+        public int postType { get; set; }
+        /// <summary>
+        /// 
+        /// </summary>
+        public int postConfig { get; set; }
+        /// <summary>
+        /// 
+        /// </summary>
+        public string dateUpdated { get; set; }
+        /// <summary>
+        /// 
+        /// </summary>
+        public int accessPermission { get; set; }
+        /// <summary>
+        /// 
+        /// </summary>
+        public bool isInSiteHome { get; set; }
+        /// <summary>
+        /// 
+        /// </summary>
+        public bool isInSiteCandidate { get; set; }
+        /// <summary>
+        /// 
+        /// </summary>
+        public bool isMarkdown { get; set; }
+    }
+
+    public class Root
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        public List<PostListItem> postList { get; set; }
+        /// <summary>
+        /// 
+        /// </summary>
+        public int postsCount { get; set; }
+        /// <summary>
+        /// 
+        /// </summary>
+        public int pageIndex { get; set; }
+        /// <summary>
+        /// 
+        /// </summary>
+        public int pageSize { get; set; }
+        /// <summary>
+        /// 
+        /// </summary>
+        public string categoryName { get; set; }
+    }
+
+
+
+
+
+
+
+
+    //如果好用，请收藏地址，帮忙分享。
+    public class BlogPost
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        public int id { get; set; }
+        /// <summary>
+        /// 
+        /// </summary>
+        public int postType { get; set; }
+        /// <summary>
+        /// 
+        /// </summary>
+        public int accessPermission { get; set; }
+        /// <summary>
+        /// Abp vNext 番外篇-疑难杂症丨浅谈扩展属性与多用户设计
+        /// </summary>
+        public string title { get; set; }
+        /// <summary>
+        /// 
+        /// </summary>
+        public string url { get; set; }
+        /// <summary>
+        /// 
+        /// </summary>
+        public string postBody { get; set; }
+        /// <summary>
+        /// 
+        /// </summary>
+        public List<int> categoryIds { get; set; }
+        /// <summary>
+        /// 
+        /// </summary>
+        public bool inSiteCandidate { get; set; }
+        /// <summary>
+        /// 
+        /// </summary>
+        public bool inSiteHome { get; set; }
+        /// <summary>
+        /// 
+        /// </summary>
+        public string siteCategoryId { get; set; }
+        /// <summary>
+        /// 
+        /// </summary>
+        public List<string> blogTeamIds { get; set; }
+        /// <summary>
+        /// 
+        /// </summary>
+        public bool isPublished { get; set; }
+        /// <summary>
+        /// 
+        /// </summary>
+        public bool displayOnHomePage { get; set; }
+        /// <summary>
+        /// 
+        /// </summary>
+        public bool isAllowComments { get; set; }
+        /// <summary>
+        /// 
+        /// </summary>
+        public bool includeInMainSyndication { get; set; }
+        /// <summary>
+        /// 
+        /// </summary>
+        public bool isPinned { get; set; }
+        /// <summary>
+        /// 
+        /// </summary>
+        public bool isOnlyForRegisterUser { get; set; }
+        /// <summary>
+        /// 
+        /// </summary>
+        public bool isUpdateDateAdded { get; set; }
+        /// <summary>
+        /// 
+        /// </summary>
+        public string entryName { get; set; }
+        /// <summary>
+        /// 
+        /// </summary>
+        public string description { get; set; }
+        /// <summary>
+        /// 
+        /// </summary>
+        public string featuredImage { get; set; }
+        /// <summary>
+        /// 
+        /// </summary>
+        public List<string> tags { get; set; }
+        /// <summary>
+        /// 
+        /// </summary>
+        public string password { get; set; }
+        /// <summary>
+        /// 
+        /// </summary>
+        public string datePublished { get; set; }
+        /// <summary>
+        /// 
+        /// </summary>
+        public bool isMarkdown { get; set; }
+        /// <summary>
+        /// 
+        /// </summary>
+        public bool isDraft { get; set; }
+        /// <summary>
+        /// 
+        /// </summary>
+        public string autoDesc { get; set; }
+        /// <summary>
+        /// 
+        /// </summary>
+        public bool changePostType { get; set; }
+        /// <summary>
+        /// 
+        /// </summary>
+        public int blogId { get; set; }
+        /// <summary>
+        /// 初久的私房菜
+        /// </summary>
+        public string author { get; set; }
+        /// <summary>
+        /// 
+        /// </summary>
+        public bool removeScript { get; set; }
+        /// <summary>
+        /// 
+        /// </summary>
+        public string clientInfo { get; set; }
+        /// <summary>
+        /// 
+        /// </summary>
+        public bool changeCreatedTime { get; set; }
+        /// <summary>
+        /// 
+        /// </summary>
+        public bool canChangeCreatedTime { get; set; }
+        /// <summary>
+        /// 
+        /// </summary>
+        public bool isContributeToImpressiveBugActivity { get; set; }
+    }
+
+    public class RootPost
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        public BlogPost blogPost { get; set; }
+    }
+
+
+
+
+
+}
---
+++ old/Easy.Core.LinuxSo/Program.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace Easy.Core.LinuxSo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+              
+
+
+
+
+
+           Console.WriteLine("Hello World!");
+        }
+    }
+
+    public class SoTester
+    {
+        private const string LibraryName = "libzmq";
+
+        const int RTLD_NOW = 2; // for dlopen's flags
+        const int RTLD_GLOBAL = 8;
+
+        [DllImport(@"libdl.so.2")]
+        public static extern IntPtr dlopen(string filename, int flags);
+        [DllImport("libdl.so.2")]
+        public static extern IntPtr dlsym(IntPtr handle, string symbol);
+
+        [DllImport("libdl.so.2", EntryPoint = "dlopen")]
+        private static extern IntPtr UnixLoadLibrary(String fileName, int flags);
+
+        [DllImport("libdl.so.2", EntryPoint = "dlclose", CallingConvention = CallingConvention.Cdecl, CharSet = CharSet.Ansi)]
+        private static extern int UnixFreeLibrary(IntPtr handle);
+
+        [DllImport("libdl.so.2", EntryPoint = "dlsym", CallingConvention = CallingConvention.Cdecl, CharSet = CharSet.Ansi)]
+        private static extern IntPtr UnixGetProcAddress(IntPtr handle, String symbol);
+
+        [DllImport("libdl.so.2", EntryPoint = "dlerror", CallingConvention = CallingConvention.Cdecl, CharSet = CharSet.Ansi)]
+        private static extern IntPtr UnixGetLastError();
+
+        public delegate int sumHandler(int a, int b);
+        public static sumHandler sumfunc = null;
+
+        [DllImport("libNativeLib.so", EntryPoint = "sum", CallingConvention = CallingConvention.Cdecl, CharSet = CharSet.Ansi)]
+        public static extern int Sum(int a, int b);
+
+        public void Start()
+        {
+            IntPtr libPtr = IntPtr.Zero;
+
+            string libName = $"{AppContext.BaseDirectory}libNativeLib.so";
+
+            libPtr = UnixLoadLibrary(libName, 2 | 8);
+
+            //libPtr = dlopen(libName, RTLD_NOW);
+
+            if (libPtr != IntPtr.Zero)
+                Console.WriteLine($"调用dlopen打开{libName}成功");
+            else
+                Console.WriteLine($"调用dlopen打开{libName}失败");
+
+            var sumPtr = UnixGetProcAddress(libPtr, "sum");
+
+            if (sumPtr != IntPtr.Zero)
+                Console.WriteLine($"dlopen调用sum成功");
+            else
+                Console.WriteLine($"dlopen调用sum失败");
+
+            sumfunc = Marshal.GetDelegateForFunctionPointer<sumHandler>(sumPtr);
+
+            int ret = sumfunc(1, 3);
+
+            Console.WriteLine($"调用sum结果:{ret}");
+
+            var sumRet = Sum(5, 7);
+
+            Console.WriteLine($"DllImport调用sum结果:{sumRet}");
+
+            //var libname2 = $"libc.so.6";
+            var libname2 = $"{AppContext.BaseDirectory}libzmq.so";
+            //var libname2 = $"{AppContext.BaseDirectory}libAdminConsole.so";
+            var consolePtr = UnixLoadLibrary(libname2, 2 | 8);
+            var erroPtr = UnixGetLastError();
+            Console.WriteLine($"错误描述：{Marshal.PtrToStringAnsi(erroPtr)}");
+
+            if (consolePtr != IntPtr.Zero)
+                Console.WriteLine($"打开{libname2}成功");
+            else
+                Console.WriteLine($"打开{libname2}失败");
+        }
+    }
+}
